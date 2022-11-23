c6c97c06a306949770f5f4f56243ccc8a11088ea
Dierdyft <diergamer123@gmail.com>
2022-11-22 20:54:10 -0500

    update

--- printNumbers.py
+++ printNumbers.py
@@ -1,26 +1,26 @@
-things = input("Put the numbers that I will loop\nThis mode: 1-3, 4-8, 9-12\n")
+things = input("Pon los numeros a los cuales recorre por su rango\nUn ejemplo: 1-3, 4-8, 9-12\n")
 
 if "," not in things:
-    print("Not there \",\" in your input")
+    print("No hay \",\" en tu entrada")
     exit()
 
 things = things.split(",")
 newThings = []
 
-#Div elements contains "-"
+#Divide los elementos que contengan "-"
 for element in things:
     newThings.append(element.split("-"))
 
-print("I printing your numbers")
-#Analysis each element in newThings    
+print("Estoy procesando los numeros")
+#Recorre cada elemento de newThings    
 for element in newThings:
-    #If the first numbers its highest than second number
+    #Si el primer numero es mayor que el segundo, no podra hacer el loop de estos
     if int(element[0]) > int(element[1]):
-            print("I cant print this numbers " + element[0] + "-" + element[1])
-    #Analysis each value in range
+            print("No puedo mostrar este intervalo de numeros " + element[0] + "-" + element[1])
+    #Recorre cada valor del elemento
     for i in range(int(element[0]), int(element[1]) + 1):
         if int(element[0]) == i:
             print(element[0] + "-" + element[1])
         print(i)
 
-print("I finished my process")
+print("He finalizado mi proceso")
