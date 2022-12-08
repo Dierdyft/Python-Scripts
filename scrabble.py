e9e2214209d1628fa1d781636eb03b9a2c4cab08
Dierdyft <diergamer123@gmail.com>
2022-12-07 20:50:49 -0500

    Update

---
+++ scrabble.py
@@ -0,0 +1,59 @@
+#Librerias necesarias
+from blessed import Terminal
+term = Terminal()
+import os
+
+#Variables necesarias
+words = []
+
+#Funcion del juego
+def game(word, lives):
+    
+    print("Intenta adivinar la palabra")
+    
+    attempts = 0
+    
+    while attempts < lives:
+        wordUser = input()
+        os.system("clear")
+
+        correctWord = word
+        correctWord = list(correctWord)
+
+        if len(wordUser) > len(correctWord):
+            print(f"No es posible jugar con una palabra de mas de {len(correctWord)} letras")
+            return
+    
+        wordUser = list(wordUser)
+    
+        i = 0
+        
+        if "".join(correctWord) == "".join(wordUser):
+            print("Haz acertado en la palabra!")
+            return
+
+        for x in wordUser:
+            if x in correctWord:
+                if x == correctWord[i]:
+                    wordUser[i] = term.green + x
+                else:
+                    wordUser[i] = term.yellow + x
+            else:
+                wordUser[i] = term.grey + x
+            i += 1
+
+        words.append(wordUser)
+
+        attempts += 1
+        
+        print(f"Intentos restantes: {attempts}")
+
+        for x in words:
+            print("".join(x))
+            
+    else:
+        print("No tienes vidas :'v")
+        return
+              
+#Iniciar el juego
+game("iglesia", 5)
